f4876f39d0ad1567445432f14806dee97ef1e91a
Matthew Hand <matthewhandau@gmail.com>
2025-01-01 05:07:16 +0000

    feat: add favicon route to serve favicon.ico from assets/images
    - Added a new view  to serve the favicon.ico file from .
    - Updated  to include a route for .

--- src/open_swarm_mcp/urls.py
+++ src/open_swarm_mcp/urls.py
@@ -6,7 +6,21 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.conf import settings
+import os
 from .rest_mode import views
+
+def favicon(request):
+    # Path to the favicon file
+    favicon_path = os.path.join(settings.BASE_DIR, 'assets', 'images', 'favicon.ico')
+    
+    # Open the file and read its contents
+    with open(favicon_path, 'rb') as f:
+        favicon_data = f.read()
+    
+    # Return the favicon data with the appropriate content type
+    return HttpResponse(favicon_data, content_type="image/x-icon")
 
 urlpatterns = [
     # Admin route
@@ -18,4 +32,7 @@ urlpatterns = [
 
     # Blueprint routes
     path('<str:blueprint_name>/', views.blueprint_webpage, name='blueprint_webpage'),
+
+    # Favicon route
+    path('favicon.ico', favicon, name='favicon'),
 ]

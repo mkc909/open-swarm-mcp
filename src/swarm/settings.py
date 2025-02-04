4090e1fe5332f3fdfb5ff1adbb5a6c2a8c883ec1
Matthew Hand <matthewhandau@gmail.com>
2025-02-04 00:09:40 +0000

    feat: enable TokenAuthentication for API security

--- src/swarm/settings.py
+++ src/swarm/settings.py
@@ -195,3 +195,16 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+INSTALLED_APPS += [
+    'rest_framework.authtoken',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}

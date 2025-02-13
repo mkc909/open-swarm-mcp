8907c3698d3b579fb6d1d4148a49bb7b582d1fe9
Matthew Hand <matthewhandau@gmail.com>
2025-02-13 21:26:36 +0000

    chore: update conftest sys.path configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.insert(0, os.path.join(os.getcwd(), "src"))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swarm.settings')
 import shutil
 from pathlib import Path

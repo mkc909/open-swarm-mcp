c70e45ee014f23a4b8ff329d470681e1ec229f63
Matthew Hand <matthewhandau@gmail.com>
2025-02-13 18:50:49 +0000

    feat(serializers): add serializers for new models

--- src/swarm/serializers.py
+++ src/swarm/serializers.py
@@ -1,7 +1,52 @@
 from rest_framework import serializers
-from .models import ChatMessage
+from .models import ChatMessage, TeachingUnit, Topic, LearningObjective, Subtopic, Course, Student, Enrollment, AssessmentItem, ChatConversation
 
 class ChatMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatMessage
+        fields = '__all__'
+
+class TeachingUnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeachingUnit
+        fields = '__all__'
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+class LearningObjectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LearningObjective
+        fields = '__all__'
+
+class SubtopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subtopic
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+class AssessmentItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssessmentItem
+        fields = '__all__'
+
+class ChatConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatConversation
         fields = '__all__'

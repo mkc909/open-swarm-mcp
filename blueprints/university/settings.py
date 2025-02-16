ab267ff485fd1fad93243bf34dff6d24094b845b
Matthew Hand <matthewhandau@gmail.com>
2025-02-16 22:14:41 +0000

    fix(university-settings): add custom label and update installed apps

--- blueprints/university/settings.py
+++ blueprints/university/settings.py
@@ -1,4 +1,21 @@
-from django.conf import settings
-from blueprints.university.apps import UniversityConfig
-if "blueprints.university" not in settings.INSTALLED_APPS:
-    settings.INSTALLED_APPS += ["blueprints.university"]
+import logging
+from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
+
+class UniversityBlueprintConfig(AppConfig):
+    name = "blueprints.university"
+    label = "blueprints_university"
+    verbose_name = "University Blueprint"
+
+default_app_config = "blueprints.university.UniversityBlueprintConfig"
+
+def update_installed_apps(settings):
+    logger.debug("University settings update: Before update, INSTALLED_APPS = %s", settings.get("INSTALLED_APPS"))
+    blueprint_app = "blueprints.university"
+    if blueprint_app not in settings.get("INSTALLED_APPS", []):
+        logger.debug("University settings update: Adding %s to INSTALLED_APPS", blueprint_app)
+        settings["INSTALLED_APPS"].append(blueprint_app)
+    else:
+        logger.debug("University settings update: %s already in INSTALLED_APPS", blueprint_app)
+# Removed automatic INSALLED_APPS update to delay it until after INSTALLED_APPS is defined in main settings.

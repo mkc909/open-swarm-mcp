3d7a886a43df2252acc9f6e5174829672f592aea
Matthew Hand <matthewhandau@gmail.com>
2025-02-24 02:52:53 +0000

    refactor: clarify app_label comment in university apps.py

--- blueprints/university/apps.py
+++ blueprints/university/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class UniversityConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "blueprints.university"
-    label = "blueprints_university"
+    name = "blueprints.university"  # Directory name
+    label = "blueprints_university"  # App label with underscore
